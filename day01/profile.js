a53c602775e9f466cb6bb83c7b6999279cb30e6a
Abdullah Al Mahmud Emon <abdullahemon6377@gmail.com>
2026-01-06 01:43:46 +0600

    Day 1: User Profile Script

---
+++ day01/profile.js
@@ -0,0 +1,32 @@
+const fullname = "Abdullah Al Mahmud Emon";
+const job = "Student";
+const country = "Bangladesh";
+const city = "Comilla";
+const age = 22;
+
+let skills = [
+    "HTML",
+    "CSS",
+    "JavaScript basics",
+    "c++",
+    "java basics",
+    "python basics",
+    "bootstrap",
+    "django basics",
+    "c",
+    "mysql",
+    "android studio basics",
+];
+
+let cfpupil='true';
+
+console.log(
+    'User Profile:\n',
+    'Full Name:', fullname, '\n',
+    'Job:', job, '\n',
+    'Country:', country, '\n',
+    'City:', city, '\n',
+    'Age:', age, '\n',
+    'Skills:', skills.join(', '), '\n',
+    'CFPupil:', cfpupil,
+)
